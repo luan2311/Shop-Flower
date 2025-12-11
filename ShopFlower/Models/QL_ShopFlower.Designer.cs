0d16f1d11d9b28ec60d6ce2543eeadace03129e7
luan2311 <phamminhluan23112005@gmail.com>
2025-12-11 23:33:23 +0700

    Modify Link href details product

--- ShopFlower/Models/QL_ShopFlower.Designer.cs
+++ ShopFlower/Models/QL_ShopFlower.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\84969\OneDrive\Máy tính\ShopFlower\ShopFlower\Models\QL_ShopFlower.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\LUAN\Downloads\Shop-Flower\ShopFlower\Models\QL_ShopFlower.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
